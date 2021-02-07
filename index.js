8473edd140d52e7e509a71bf7be1479483a2ef77
JGullickson <Jodi.Gullickson@gmail.com>
2021-02-07 15:33:04 -0600

    Comments of what JS I want Added

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-$('.mockup').on('hover', function(){
-    $('.leftHide').fadeIn();
+// Create a variable below
+var helloWorld = "Hello World!";
+
+// Console.log() your variable here.
+console.log(helloWorld);
+
+
+/*
+ways we tried to reveal the hidden div in work section
+
+$(document).ready(function(){
+    $('.t4t').hover(function(){
+        $('.hideT4t').removeClass('hide');
+    })
 });
+
+$('.t4t').on('hover', function() {
+    $('.hideT4t').fadeIn(1000);
+});
+
+$('.t4t').on('mouseover', function() {
+    $('.hideT4t').stop().show(1000);
+});
+
+$("#t4t").mouseenter(function(){
+    $(".hideT4t").show();
+  });
+  $("#show").mouseleave(function(){
+    $("hideT4t").hide();
+  });
+  
+  */
