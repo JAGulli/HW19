fac05148ef8951ced18d55a974cb63cdadbd35cd
JGullickson <Jodi.Gullickson@gmail.com>
2021-02-08 22:37:01 -0600

    Add Contact Form

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
     setInterval(callMe,1250);
 
 // RETURN TO TOP Script
-      //Get the button:
+  //Get the button:
   mybutton = document.getElementById("rtnTop");
 
   // When the user scrolls down 20px from the top of the document, show the button
